12a7fe18361869c7f59d84ae7f4cf641ab237254
vasilisa <igorevna.vasilisa@gmail.com>
2016-11-15 19:17:57 +0200

    added JS file

---
+++ js/index.js
@@ -0,0 +1,15 @@
+console.log(document.title);
+/*$(function() {
+  // Get page title
+  var pageTitle = $("title").text();
+
+  // Change page title on blur
+  $(window).blur(function() {
+    $("title").text("Don't forget to read this...");
+  });
+
+  // Change page title back on focus
+  $(window).focus(function() {
+    $("title").text(pageTitle);
+  });
+});*/
